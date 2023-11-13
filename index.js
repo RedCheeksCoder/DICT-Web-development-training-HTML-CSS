4fd4ec1c1d98cf34b4c743873cfd8fd6600c0786
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2023-11-13 23:23:41 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    id: 0,
+    title: "Affogato",
+    price: 195,
+  },
+  {
+    id: 1,
+    title: "Espresso",
+    price: 50,
+  },
+  {
+    id: 2,
+    title: "Mocha",
+    price: 180,
+  },
+  {
+    id: 3,
+    title: "Flat White",
+    price: 170,
+  },
+  {
+    id: 4,
+    title: "Cappuccino",
+    price: 205,
+  },
+  {
+    id: 5,
+    title: "Palazzo",
+    price: 145,
+  },
+  {
+    id: 6,
+    title: "Cortado",
+    price: 210,
+  },
+  {
+    id: 7,
+    title: "Ristretto",
+    price: 195,
+  },
+  {
+    id: 8,
+    title: "Americano",
+    price: 170,
+  },
+];
+
+let cart = [];
+
+function addToCart(id) {
+  cart.push({ ...products[id] });
+  console.log(cart);
+  displaycart();
+}
+
+function delElement(a) {
+  cart.splice(a, 1);
+  /* displaycart(); */
+}
+let finalTotal = 0;
+function displaycart() {
+  let total = 0;
+  let j = 0;
+  if (cart.length == 0) {
+    document.getElementById("totalPrice").innerHTML = "PHP " + 0 + ".00";
+    document.getElementById(
+      "orders"
+    ).innerHTML = `<div style="text-align: center"><p>"Your cart is empty"</p></div>`;
+  } else {
+    document.getElementById("orders").innerHTML = cart
+      .map((items) => {
+        let { title, price } = items;
+        total = total + price;
+        finalTotal = total;
+        document.getElementById("totalPrice").innerHTML =
+          "PHP " + total + ".00";
+        return (
+          `<div class='orders-container'>
+                  <p style='font-size: 1rem;'>${title}</p>
+                  <h2 style='font-size: 1rem;'>PHP ${price}.00</h2>` +
+          "<i class='fa-solid fa-trash' onclick='delElement(" +
+          j++ +
+          ")'></i></div>"
+        );
+      })
+      .join("");
+  }
+}
+let receipt = "";
+function displayReceipt() {
+  cart.map((items) => {
+    let { title } = items;
+    receipt += `${title} \n`;
+  });
+  alert(`ORDER RECEIPT: \n${receipt} \n TOTAL: PHP ${finalTotal}`);
+  document.getElementById(
+    "orders"
+  ).innerHTML = `<div style="text-align: center"><p>"Your cart is empty"</p></div>`;
+}
